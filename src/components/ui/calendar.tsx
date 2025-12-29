e37b07b83c357ac9a1cce9df01f45e9ff6ae6062
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2025-12-29 21:42:33 +0000

    23:40:52.266 Running build in Washington, D.C., USA (East) – iad1 23:40:

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        icon_left: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        icon_right: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
